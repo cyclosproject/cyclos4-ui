587c8d9345ba04a70df4183e8f872bc74bd1ac1a
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2018-07-12 10:11:50 -0300

    Added geocoding on the registration form

--- src/app/users/registration/registration-fields.component.ts
+++ src/app/users/registration/registration-fields.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injector, ChangeDetectionStrategy, Input } from '@angular/core';
 import { BaseComponent } from 'app/shared/base.component';
-import { UserDataForNew } from 'app/api/models';
+import { UserDataForNew, GeographicalCoordinate } from 'app/api/models';
 import { FormGroup } from '@angular/forms';
 import { AvailabilityEnum } from 'app/api/models/availability-enum';
 import { AddressFieldEnum } from 'app/api/models/address-field-enum';
@@ -9,6 +9,8 @@ import { CountriesResolve } from 'app/countries.resolve';
 import { Observable } from 'rxjs';
 import { Country } from 'app/api/models/country';
 import { BehaviorSubject } from 'rxjs';
+import { MapsService } from 'app/core/maps.service';
+import { debounceTime } from 'rxjs/operators';
 
 /**
  * Provides the input for user fields
@@ -16,12 +18,14 @@ import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'registration-fields',
   templateUrl: 'registration-fields.component.html',
+  styleUrls: ['registration-fields.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegistrationFieldsComponent extends BaseComponent {
   constructor(
     injector: Injector,
-    private countriesResolve: CountriesResolve) {
+    private countriesResolve: CountriesResolve,
+    public maps: MapsService) {
     super(injector);
   }
 
@@ -33,6 +37,36 @@ export class RegistrationFieldsComponent extends BaseComponent {
 
   @Input()
   data: UserDataForNew;
+
+  location = new BehaviorSubject<GeographicalCoordinate>(null);
+
+  ngOnInit() {
+    super.ngOnInit();
+    if (this.maps.enabled) {
+      this.subscriptions.push(this.location.subscribe(loc =>
+        this.addressForm.patchValue({ location: loc })));
+      this.subscriptions.push(this.addressForm.valueChanges.pipe(
+        debounceTime(ApiHelper.DEBOUNCE_TIME)
+      ).subscribe(value => {
+        // Only attempt to geocode if there is at least the address line 1 or street name
+        if (value.addressLine1 || value.street) {
+          const fields: string[] = [];
+          for (const field of this.data.addressConfiguration.enabledFields) {
+            if (field === AddressFieldEnum.COMPLEMENT || field === AddressFieldEnum.PO_BOX) {
+              // These fields are not useful for geocoding
+              continue;
+            }
+            fields.push(value[field]);
+          }
+          this.subscriptions.push(this.maps.geocode(fields).subscribe(loc => {
+            this.location.next(loc);
+          }));
+        } else {
+          this.location.next(null);
+        }
+      }));
+    }
+  }
 
   get hasAddress(): boolean {
     return this.data.addressConfiguration.availability !== AvailabilityEnum.DISABLED;
