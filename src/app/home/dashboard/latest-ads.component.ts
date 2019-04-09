0e11f019a9f4f717c56a831f3fc37da187005c38
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2019-04-09 09:15:52 -0300

    Click on user icon in latest ads -> view profile

--- src/app/home/dashboard/latest-ads.component.ts
+++ src/app/home/dashboard/latest-ads.component.ts
@@ -98,7 +98,7 @@ export class LatestAdsComponent extends BaseDashboardComponent implements OnInit
 
   navigateToOwner(ad: AdResult, event: MouseEvent) {
     this.menu.navigate({
-      url: this.path(ad),
+      url: `/users/profile/${ad.owner.id}`,
       menu: new ActiveMenu(Menu.USER_PROFILE),
       clear: false,
       event: event
