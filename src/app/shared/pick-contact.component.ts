d3a015cd2890b11268f3f10eebb7d255dafbb5b0
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2019-04-15 14:46:30 -0300

    Implemented keyboard navigation on pick contacts

--- src/app/shared/pick-contact.component.ts
+++ src/app/shared/pick-contact.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
-import { User } from 'app/api/models';
+import { ChangeDetectionStrategy, Component, EventEmitter, Injector, OnInit, Output, ElementRef } from '@angular/core';
+import { User, Contact } from 'app/api/models';
 import { ContactsService } from 'app/api/services';
 import { ApiHelper } from 'app/shared/api-helper';
 import { BaseComponent } from 'app/shared/base.component';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { focus } from 'app/shared/helper';
 
 /**
  * A component to be shown in a dialog, showing the user contact list
@@ -26,6 +27,7 @@ export class PickContactComponent extends BaseComponent implements OnInit {
   constructor(
     injector: Injector,
     private contactsService: ContactsService,
+    private element: ElementRef,
     public modalRef: BsModalRef
   ) {
     super(injector);
@@ -39,11 +41,56 @@ export class PickContactComponent extends BaseComponent implements OnInit {
     }).subscribe(c => {
       this.contacts$.next(c || []);
     }));
+    this.addShortcut(['ArrowUp', 'ArrowDown', 'Enter', 'Escape'], event => {
+      if (event.key === 'Escape') {
+        this.close();
+        return;
+      }
+
+      const element = this.element.nativeElement as HTMLElement;
+      const active = document.activeElement as HTMLElement;
+      let index = -1;
+      active.classList.forEach(c => {
+        const match = c.match(/contact\-link\-(\d+)/);
+        if (match) {
+          index = Number.parseInt(match[1], 10);
+        }
+      });
+
+      const contacts = this.contacts$.value;
+      switch (event.key) {
+        case 'ArrowUp':
+          index--;
+          break;
+        case 'ArrowDown':
+          index++;
+          break;
+        case 'Enter':
+          if (index >= 0) {
+            const contact = contacts[index] as Contact;
+            this.emit(contact.contact);
+          }
+          return;
+      }
+
+      index = Math.min(Math.max(0, index), contacts.length - 1);
+      const toFocus = element.getElementsByClassName(`contact-link-${index}`);
+      if (toFocus.length > 0) {
+        focus(toFocus.item(0));
+      }
+    });
   }
 
-  emit(contact: User, event: MouseEvent) {
+  emit(contact: User, event?: MouseEvent) {
     this.select.emit(contact);
-    event.preventDefault();
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.close();
+  }
+
+  close() {
     this.modalRef.hide();
   }
 
