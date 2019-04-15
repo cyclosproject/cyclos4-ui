192ac5dec29ef513b958fc796bf242db7784489f
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2019-04-15 10:01:20 -0300

    Added keyboard shortcuts to result type field

--- src/app/shared/result-type-field.component.ts
+++ src/app/shared/result-type-field.component.ts
@@ -1,11 +1,13 @@
 import {
-  Component, Input, Output, EventEmitter, ChangeDetectionStrategy, SkipSelf, Host, Optional, OnInit, Injector
+  ChangeDetectionStrategy, Component, ElementRef, EventEmitter,
+  Host, Injector, Input, OnInit, Optional, Output, SkipSelf
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR, FormControl, ControlContainer } from '@angular/forms';
-import { BaseControlComponent } from 'app/shared/base-control.component';
-import { ResultType } from 'app/shared/result-type';
+import { ControlContainer, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MapsService } from 'app/core/maps.service';
+import { BaseControlComponent } from 'app/shared/base-control.component';
 import { LayoutService } from 'app/shared/layout.service';
+import { ResultType } from 'app/shared/result-type';
+import { Subscription } from 'rxjs';
 
 /**
  * Renders a widget for a result type field
@@ -19,7 +21,10 @@ import { LayoutService } from 'app/shared/layout.service';
     { provide: NG_VALUE_ACCESSOR, useExisting: ResultTypeFieldComponent, multi: true }
   ]
 })
-export class ResultTypeFieldComponent extends BaseControlComponent<ResultType> implements OnInit {
+export class ResultTypeFieldComponent
+  extends BaseControlComponent<ResultType>
+  implements OnInit {
+
   @Input() name: string;
   @Input() required: boolean;
   @Input() placeholder: string;
@@ -31,11 +36,14 @@ export class ResultTypeFieldComponent extends BaseControlComponent<ResultType> i
   @Output() change: EventEmitter<string> = new EventEmitter();
   @Output() blur: EventEmitter<string> = new EventEmitter();
 
+  private resultTypeSubs = new Map<ResultType, Subscription>();
+
   constructor(
     injector: Injector,
     @Optional() @Host() @SkipSelf() controlContainer: ControlContainer,
     public layout: LayoutService,
-    private maps: MapsService
+    private maps: MapsService,
+    private elementRef: ElementRef
   ) {
     super(injector, controlContainer);
   }
@@ -60,6 +68,50 @@ export class ResultTypeFieldComponent extends BaseControlComponent<ResultType> i
         return 'view_list';
       case ResultType.MAP:
         return 'place';
+    }
+  }
+
+  addShortcuts(resultType: ResultType) {
+    const element = this.elementRef.nativeElement as HTMLElement;
+    const currentEl = element.getElementsByClassName(`resultType-${resultType}`).item(0);
+    if (document.activeElement !== currentEl) {
+      return;
+    }
+    const keys = ['ArrowLeft', 'ArrowRight', ' ', 'Enter'];
+    const sub = this.shortcut.subscribe(keys, event => {
+      let index = this.allowedResultTypes.indexOf(resultType);
+      let alsoClick = false;
+      switch (event.key) {
+        case 'ArrowLeft':
+          index--;
+          break;
+        case 'ArrowRight':
+          index++;
+          break;
+        case ' ':
+        case 'Enter':
+          alsoClick = true;
+          break;
+      }
+      index = Math.min(Math.max(0, index), this.allowedResultTypes.length - 1);
+      const newResultType = this.allowedResultTypes[index];
+      const toFocus = element.getElementsByClassName(`resultType-${newResultType}`);
+      if (toFocus.length > 0) {
+        const focusEl = toFocus.item(0) as HTMLElement;
+        focusEl.focus();
+        if (alsoClick) {
+          focusEl.click();
+        }
+      }
+    });
+    this.resultTypeSubs.set(resultType, sub);
+  }
+
+  removeShortcuts(resultType: ResultType) {
+    const sub = this.resultTypeSubs.get(resultType);
+    if (sub) {
+      sub.unsubscribe();
+      this.resultTypeSubs.delete(resultType);
     }
   }
 }
