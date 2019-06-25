f98cd8d9f336ce9af20905954febd538998e5884
Andrés Meyer <andres@cyclos.org>
2019-06-25 10:54:14 -0300

    buy vouchers

--- src/app/banking/vouchers/buy-voucher.component.ts
+++ src/app/banking/vouchers/buy-voucher.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component, Injector, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Injector,
+  OnInit
+} from '@angular/core';
 import { VoucherDataForBuy } from 'app/api/models/voucher-data-for-buy';
 import { VouchersService } from 'app/api/services';
 import { BasePageComponent } from 'app/shared/base-page.component';
 import { BehaviorSubject } from 'rxjs';
 import { Currency } from 'app/api/models';
+import { FormGroup, FormControl } from '@angular/forms';
 
 export type BuyVoucherStep = 'form' | 'confirm' | 'done';
 
@@ -12,13 +18,16 @@ export type BuyVoucherStep = 'form' | 'confirm' | 'done';
   templateUrl: 'buy-voucher.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BuyVoucherComponent extends BasePageComponent<VoucherDataForBuy> implements OnInit {
+export class BuyVoucherComponent extends BasePageComponent<VoucherDataForBuy>
+  implements OnInit {
   step$ = new BehaviorSubject<BuyVoucherStep>(null);
   /**
    * The voucher type
    */
   typeParam: string;
   userParam: string;
+
+  form: FormGroup;
 
   constructor(injector: Injector, private voucherService: VouchersService) {
     super(injector);
@@ -35,10 +44,17 @@ export class BuyVoucherComponent extends BasePageComponent<VoucherDataForBuy> im
     super.ngOnInit();
     const params = this.route.snapshot.params;
     this.typeParam = params.type;
-    this.userParam = this.authHelper.isSelf(params.user) ? this.ApiHelper.SELF : params.user;
+    this.userParam = this.authHelper.isSelf(params.user)
+      ? this.ApiHelper.SELF
+      : params.user;
 
-    this.voucherService.getVoucherDataForBuy({ user: this.userParam, type: this.typeParam })
-      .subscribe(data => this.data = data);
+    this.voucherService
+      .getVoucherDataForBuy({ user: this.userParam, type: this.typeParam })
+      .subscribe(data => (this.data = data));
+    this.form = this.formBuilder.group({
+      count: new FormControl(''),
+      amount: new FormControl('')
+    });
   }
 
   onDataInitialized() {
