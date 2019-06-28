3698ee07f76c1ca56b71cc00f7c1f197ddc9f237
Augusto Castro <isaugustocastro@gmail.com>
2019-06-28 11:08:22 -0300

    Issue 132 (#134)
    * ready
    
    * show voucher code input as required in redeem voucher
    
    * Fixed i18n
    
    Fixes #132

--- src/app/core/format.service.ts
+++ src/app/core/format.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiConfiguration } from 'app/api/api-configuration';
-import { Currency, DataForUi } from 'app/api/models';
+import { Currency, DataForUi, WeekDayEnum } from 'app/api/models';
 import { I18n } from 'app/i18n/i18n';
 import Big from 'big.js';
 import moment from 'moment-mini-ts';
@@ -440,4 +440,22 @@ export class FormatService {
     return this.minWeekdayName[weekday];
   }
 
+  weekDay(day: WeekDayEnum): string {
+    switch (day) {
+      case WeekDayEnum.FRI:
+        return this.i18n.general.weekday.long.fri;
+      case WeekDayEnum.MON:
+        return this.i18n.general.weekday.long.mon;
+      case WeekDayEnum.SAT:
+        return this.i18n.general.weekday.long.sat;
+      case WeekDayEnum.SUN:
+        return this.i18n.general.weekday.long.sun;
+      case WeekDayEnum.THU:
+        return this.i18n.general.weekday.long.thu;
+      case WeekDayEnum.TUE:
+        return this.i18n.general.weekday.long.tue;
+      case WeekDayEnum.WED:
+        return this.i18n.general.weekday.long.wed;
+    }
+  }
 }
