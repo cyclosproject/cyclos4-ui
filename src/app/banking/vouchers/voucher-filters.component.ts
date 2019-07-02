407618506f5391d53102c018b355cf9cac78e9f1
Augusto Castro <augusto@cyclos.org>
2019-07-02 10:32:57 -0300

    created voucher filters

--- src/app/banking/vouchers/voucher-filters.component.ts
+++ src/app/banking/vouchers/voucher-filters.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Injector, Input } from '@angular/core';
 import {
-  VouchersDataForSearch, VoucherStatusEnum, VoucherCreationTypeEnum, UserVouchersDataForSearch, TransferFilter
+  VouchersDataForSearch, VoucherStatusEnum, VoucherCreationTypeEnum, UserVouchersDataForSearch, Group
 } from 'app/api/models';
 import { FieldOption } from 'app/shared/field-option';
 import { BankingHelperService } from 'app/core/banking-helper.service';
@@ -14,14 +14,15 @@ import { FormGroup } from '@angular/forms';
 })
 export class VoucherFiltersComponent extends BaseComponent implements OnInit {
 
-  @Input() data: VouchersDataForSearch;
+  @Input() adminData: VouchersDataForSearch;
   @Input() userData: UserVouchersDataForSearch;
   @Input() heading: string;
   @Input() mobileHeading: string;
   @Input() form: FormGroup;
-  @Input() transferFilters: TransferFilter[];
 
   mask: string;
+  isVoucherSearch: boolean;
+  isUserVoucherSearch: boolean;
   constructor(
     injector: Injector,
     private bankingHelper: BankingHelperService
@@ -31,8 +32,13 @@ export class VoucherFiltersComponent extends BaseComponent implements OnInit {
 
   ngOnInit() {
     super.ngOnInit();
+    this.isUserVoucherSearch = !!this.userData;
+    this.isVoucherSearch = !!this.adminData;
   }
 
+  get data(): (VouchersDataForSearch | UserVouchersDataForSearch) {
+    return this.adminData ? this.adminData : this.userData;
+  }
   get statusOptions(): FieldOption[] {
     const statuses = Object.values(VoucherStatusEnum) as VoucherStatusEnum[];
     return statuses.map(st => ({ value: st, text: this.bankingHelper.voucherStatus(st) }));
@@ -42,6 +48,16 @@ export class VoucherFiltersComponent extends BaseComponent implements OnInit {
     const statuses = Object.values(VoucherCreationTypeEnum) as VoucherCreationTypeEnum[];
     return statuses.map(st => ({ value: st, text: this.bankingHelper.voucherCreationType(st) }))
       .concat({ value: null, text: this.i18n.general.all });
+  }
+
+  get userGroups(): Group[] {
+    return this.isVoucherSearch ? this.adminData.userGroups : [];
+  }
+
+  get maxScale(): number {
+    return this.adminData.types
+      .map(t => t.configuration.currency.decimalDigits)
+      .reduce((previous, current, _index, _array) => previous > current ? previous : current);
   }
 
 }
