b83b6c4d7348ea2240c4ec57fdf5a78f82c3c216
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2019-07-05 13:36:59 -0300

    Allow setting labelPosition on custom-field-value

--- src/app/shared/custom-field-value.component.ts
+++ src/app/shared/custom-field-value.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input, ViewChild } from '@angular/c
 import { CustomField, CustomFieldTypeEnum, CustomFieldValue } from 'app/api/models';
 import { LayoutService } from 'app/shared/layout.service';
 import { FormatFieldValueComponent } from './format-field-value.component';
+import { FieldLabelPosition } from 'app/shared/base-form-field.component';
 
 /**
  * Component used to display a custom field value as a `<label-value>`
@@ -14,6 +15,7 @@ import { FormatFieldValueComponent } from './format-field-value.component';
 })
 export class CustomFieldValueComponent {
 
+  @Input() labelPosition: FieldLabelPosition;
   @Input() fieldValue: CustomFieldValue;
 
   @ViewChild('formatFieldValue', { static: true }) formatFieldValue: FormatFieldValueComponent;
@@ -25,13 +27,18 @@ export class CustomFieldValueComponent {
     return this.fieldValue ? this.fieldValue.field : null;
   }
 
-  labelOnTop(ltsm: boolean): boolean {
-    if (!ltsm) {
-      return false;
+  getLabelPosition(ltsm: boolean): FieldLabelPosition {
+    if (this.labelPosition) {
+      return this.labelPosition;
+    }
+    if (ltsm) {
+      return 'auto';
     }
     const field = this.field;
     const type = field ? field.type : null;
-    return [CustomFieldTypeEnum.RICH_TEXT, CustomFieldTypeEnum.TEXT].includes(type);
+    // Show rich text / text with label above
+    return [CustomFieldTypeEnum.RICH_TEXT, CustomFieldTypeEnum.TEXT].includes(type)
+      ? 'above' : 'auto';
   }
 
 }
