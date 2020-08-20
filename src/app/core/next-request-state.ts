297960ef3fba1c91f5479be8a7fcec0b825b9e11
Luis Fernando Planella Gonzalez <lfpg.dev@gmail.com>
2020-08-20 11:52:49 -0300

    Increased a bit the timeout for requests

--- src/app/core/next-request-state.ts
+++ src/app/core/next-request-state.ts
@@ -101,12 +101,12 @@ export class NextRequestState {
     // Append the resulting request in the pending list
     this.pending$.next([result, ...this.pending$.value]);
 
-    // Just as a fallback, after 7 seconds, remove the request from the pending list
+    // Just as a fallback, after a few seconds, remove the request from the pending list
     setTimeout(() => {
       if (this.pending$.value.includes(result)) {
         this.pending$.next(this.pending$.value.filter(r => r !== result));
       }
-    }, 7000);
+    }, 12000);
 
     return result;
   }
