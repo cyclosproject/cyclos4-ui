4178a484b1bdf90bf10a42d39e665496e4ff0c55
Luis Fernando Planella Gonzalez <luis@cyclos.org>
2021-05-24 09:46:41 -0300

    Added some missing files

---
+++ src/app/shared/information-text.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { NotificationService } from 'app/core/notification.service';
+import { SvgIcon } from 'app/core/svg-icon';
+
+/**
+ * Show the given information text on a tooltip or in a notification when clicked
+ */
+@Component({
+  selector: 'information-text',
+  templateUrl: 'information-text.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InformationTextComponent implements OnInit {
+
+  @Input() informationText: string;
+
+  constructor(private notification: NotificationService) {
+  }
+
+  ngOnInit() {
+  }
+
+  get icon(): SvgIcon {
+    return SvgIcon.InfoCircle;
+  }
+
+  onClick() {
+    this.notification.info(this.informationText);
+  }
+}
