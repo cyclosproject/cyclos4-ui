c7b07b37f12397cb930bd7f375e748e265bd269a
Luis Fernando Planella Gonzalez <luis@cyclos.org>
2022-02-25 09:18:43 -0300

    4.15.2-dev

---
+++ src/app/ui/dashboard/quick-access-action.ts
@@ -0,0 +1,9 @@
+import { SvgIcon } from 'app/core/svg-icon';
+import { MenuEntry } from 'app/ui/shared/menu';
+
+export interface QuickAccessAction {
+  icon: SvgIcon;
+  label: string;
+  entry: MenuEntry;
+  onClick?: () => void;
+}
